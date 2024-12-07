69d10763518530c7cc1a6f4ac39a247e64ccf5cd
Shehab Anwar <126429412+Shehab-Anwar-X@users.noreply.github.com>
2024-12-07 16:51:13 +0200

    F

--- lib/features/sign_up/domain/repositories/signup_repository.dart
+++ lib/features/sign_up/domain/repositories/signup_repository.dart
@@ -1,11 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:qubeCommerce/core/api/success_response.dart';
 
 import '../../../../core/error/Failure.dart';
-import 'package:dartz/dartz.dart';
-
 import '../entities/register_parameter.dart';
 
 abstract class SignUpRepository {
-  Future<Either<Failure, ApiResponse>> register(
-      {required RegisterParameter registerParameter});
+  Future<Either<Failure, ApiResponse>> register({
+    required RegisterParameter registerParameter,
+  });
 }
